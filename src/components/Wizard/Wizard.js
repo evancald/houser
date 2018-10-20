f1560cf84ff968e37aa75ee07a41599a10eb121c
Evan Caldwell <evancald@sas.upenn.edu>
2018-10-20 10:53:58 -0600

    completed part 1 step 2, added event handlers for wizard inputs

--- src/components/Wizard/Wizard.js
+++ src/components/Wizard/Wizard.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react';
 
 class Wizard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      address: '',
+      city: '',
+      state: '',
+      zip: 99999
+    }
+  }
+
+  handleChange = (value, toUpdate) => {
+    this.setState({[toUpdate]: value});
+  }
 
   cancel = () => {
     this.props.history.push('/');
@@ -11,6 +25,11 @@ class Wizard extends Component {
       <div>
         Wizard
         <button onClick={() => this.cancel()}>Cancel</button>
+        <input onChange={(e) => this.handleChange(e.target.value, 'name')} value={this.state.name} placeholder='name'></input>
+        <input onChange={(e) => this.handleChange(e.target.value, 'address')} value={this.state.address} placeholder='address'></input>
+        <input onChange={(e) => this.handleChange(e.target.value, 'city')} value={this.state.city} placeholder='city'></input>
+        <input onChange={(e) => this.handleChange(e.target.value, 'state')} value={this.state.state} placeholder='state'></input>
+        <input onChange={(e) => this.handleChange(e.target.value, 'zip')} value={this.state.zip} placeholder='zip'></input>
       </div>
     )
   }
